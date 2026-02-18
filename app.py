c83db36049be0e33a4b59058222e844c195a931c
Siddu oggu <sidduoggu8@gmail.com>
2026-02-18 21:27:33 +0530

    fix render start

--- app.py
+++ app.py
@@ -124,3 +124,6 @@ def clear():
     conn.commit()
     conn.close()
     return "Complaints cleared successfully!"
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
