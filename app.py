c6df4d25b73d79d16dc6d2a988f96751033e10f0
Siddu oggu <sidduoggu8@gmail.com>
2026-02-21 20:38:07 +0530

    UI improved + security fixed

--- app.py
+++ app.py
@@ -192,5 +192,4 @@ def logout():
     return redirect("/login")
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port)
+    app.run(debug=True)
